b0d1dedaacd50f63170c12fcc6e3273c81ce2531
Matt McFarland <783024+vanetix@users.noreply.github.com>
2018-01-25 17:33:08 -0600

    Add integration for lambda runtime

--- main.go
+++ main.go
@@ -3,63 +3,84 @@ package main
 import (
 	"compress/gzip"
 	"fmt"
-	//"github.com/aws/aws-sdk-go/aws"
-	//"github.com/aws/aws-sdk-go/aws/session"
-	//"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/CreditCardsCom/bushwack/bushwack"
-	"github.com/Jeffail/tunny"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
-	"path/filepath"
 	"strings"
 )
 
-const urlFormat = "<Elastic search endpoint>"
+var esUrl string
+
+const esHostDefault = "https://vpc-logstash-dev-vpc-uflljt22oi3shmsevidb235hxq.us-west-2.es.amazonaws.com"
+
+func init() {
+	host := os.Getenv("ES_HOST")
+	if host == "" {
+		host = esHostDefault
+		log.Println("ES_HOST was not set!")
+	}
+
+	esUrl = fmt.Sprintf("%s/_bulk", host)
+}
 
 func main() {
-	//sess, err := session.NewSession(&aws.Config{Region: aws.String("us-west-2")})
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
+	lambda.Start(EventHandler)
+}
 
-	//service := s3.New(sess)
-	//s3Resp, err := service.ListObjects(&s3.ListObjectsInput{
-	//	Bucket: aws.String("cccom-elb-logs"),
-	//	Prefix: aws.String("transnode/AWSLogs/799335648850/elasticloadbalancing/us-west-2/2017/01/23/"),
-	//})
-
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-
-	//for _, item := range s3Resp.Contents {
-	//	fmt.Println("Name:         ", *item.Key)
-	//	fmt.Println("Last modified:", *item.LastModified)
-	//	fmt.Println("Size:         ", *item.Size)
-	//	fmt.Println("Storage class:", *item.StorageClass)
-	//	fmt.Println("")
-	//}
-
-	pool := tunny.NewFunc(20, func(p interface{}) interface{} {
-		processLog(p.(string))
-
-		return nil
-	})
-	defer pool.Close()
-
-	files, err := ioutil.ReadDir("logs")
+func EventHandler(event events.S3Event) {
+	sess, err := session.NewSession(&aws.Config{Region: aws.String("us-west-2")})
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	for _, f := range files {
-		if !f.IsDir() {
-			p := filepath.Join("logs", f.Name())
+	dlManager := s3manager.NewDownloader(sess)
+	var objects []s3manager.BatchDownloadObject
+	var files []string
 
-			pool.Process(p)
+	// Build a batch download of all objects send in the event
+	for _, r := range event.Records {
+		e := r.S3
+
+		// Verify we have what we thing we have...
+		if !strings.HasSuffix(e.Object.Key, ".log.gz") {
+			continue
 		}
+
+		tf, err := ioutil.TempFile("", "")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer tf.Close()
+		defer os.Remove(tf.Name())
+
+		o := s3manager.BatchDownloadObject{
+			Writer: tf,
+			Object: &s3.GetObjectInput{
+				Bucket: &e.Bucket.Name,
+				Key:    &e.Object.Key,
+			},
+		}
+
+		files = append(files, tf.Name())
+		objects = append(objects, o)
+	}
+
+	it := &s3manager.DownloadObjectsIterator{Objects: objects}
+	ctx := aws.BackgroundContext()
+	if err := dlManager.DownloadWithIterator(ctx, it); err != nil {
+		log.Fatal(err)
+	}
+
+	for _, f := range files {
+		processLog(f)
 	}
 }
 
@@ -79,8 +100,7 @@ func processLog(filename string) {
 		log.Fatal(err)
 	}
 
-	url := fmt.Sprintf(urlFormat, "2018.01.23")
-	resp, err := http.Post(url, "application/x-ndjson", strings.NewReader(body))
+	resp, err := http.Post(esUrl, "application/x-ndjson", strings.NewReader(body))
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -89,7 +109,7 @@ func processLog(filename string) {
 		log.Println("Yikes, looks like we had a request return a non-200 status")
 	}
 
-	log.Printf("Sent off %d log entires.", len(entries))
+	log.Printf("Sent off %d log entries.", len(entries))
 }
 
 func decompress(f string) ([]byte, error) {
